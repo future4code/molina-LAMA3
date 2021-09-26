e8009e6292dd8cbf132d227dca47d1413e558d5a
Bruno Moreira Santos <brms@MBP-de-Bruno.br>
2021-09-25 21:44:21 -0300

    Endpoint signup completo

---
+++ src/controller/UserController.ts
@@ -0,0 +1,36 @@
+import { Request, Response } from "express";
+import { SignupDTO, UserBusiness } from "../business/UserBusiness";
+
+export class UserController {
+    private userBusiness: UserBusiness;
+
+    constructor() {
+        // Instanciando o business
+        this.userBusiness = new UserBusiness();
+    }
+
+    async signup(req: Request, res: Response) {
+        try {
+            // Monta o DTO
+            const signupDTO: SignupDTO = {
+                email: req.body.email,
+                name: req.body.name,
+                password: req.body.password,
+                role: req.body.role
+            };
+
+            // Validação do DTO
+            if (!signupDTO.email || !signupDTO.name || !signupDTO.password) {
+                throw new Error("Algum parâmetro faltando.");
+            }
+
+            // Invoca o caso de uso
+            const output = await this.userBusiness.signup(signupDTO)
+
+            res.send({ token: output.token })
+            
+        } catch (error: any) {
+            res.status(500).send(error.message)
+        }
+    }
+}
---
+++ src/data/UserDatabase.ts
@@ -0,0 +1,38 @@
+import { stringToUserRole, User } from "../business/entities/User";
+import { BaseDatabase } from "./BaseDatabase";
+
+export class UserDatabase extends BaseDatabase {
+    public async save(user: User) {
+        const userDB = {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            password: user.passwordHash,
+            role: stringToUserRole(user.role)
+        }
+
+        await BaseDatabase.connection("LAMA_USUÁRIOS").insert(userDB).onConflict('id').merge();
+
+        return user
+    }
+
+    async findUserByEmail(email: string) {
+        const queryResponse = await BaseDatabase.connection("LAMA_USUÁRIOS").select('*').where({email: email})
+    
+        const userDB = queryResponse[0]
+
+        if(!userDB) {
+            return null
+        }
+
+        const user: User = {
+            id: userDB.id,
+            name: userDB.name,
+            email: userDB.email,
+            passwordHash: userDB.password_hash,
+            role: stringToUserRole(userDB.role)
+        }
+
+        return user
+    }
+}
